19d5b6c6e189632c6beca40df70c563d1c37b3d1
Wer Degars <werdegars@gmail.com>
2024-01-03 17:37:54 +0700

    Add by708.is-an.app

---
+++ domains/by708.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Home IOT projects',
+  domain: 'is-an.app',
+  subdomain: 'by708',
+  owner: {
+    email: 'werdegars@gmail.com',
+  },
+  record: {
+    NS: ['ivan.ns.cloudflare.com', 'jean.ns.cloudflare.com']
+  },
+  proxy: false
+});
